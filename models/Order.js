63647b9405e3f64b6a72f985ca87e53ee84dbff0
Azra Hadžihajdarević <51314049+AzraHa@users.noreply.github.com>
2021-01-25 16:50:27 +0100

    navigation,order

--- models/Order.js
+++ models/Order.js
@@ -5,9 +5,10 @@ const orderSchema = new mongoose.Schema({
   customer:[{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
   restaurant: [{ type: mongoose.Schema.Types.ObjectId, ref: "Restaurant" }],
   food:[{ type: mongoose.Schema.Types.ObjectId, ref: "Food" }],
+  status:{type:Boolean,required:true},
   date: {type: Date, default: Date.now}
 });
 
-const Order = mongoose.model('orderSchema', orderSchema);
+const Order = mongoose.model('order', orderSchema);
 
 module.exports = Order;
--- routes/users.js
+++ routes/users.js
@@ -5,6 +5,7 @@ const Food = require('../models/Food');
 const FoodType = require('../models/FoodType');
 const Sale = require('../models/Sale');
 const Restaurant = require("../models/Restaurant");
+const Order = require("../models/Order");
 
 
 router.get('/login',userController.login_get);
@@ -16,20 +17,37 @@ router.get('/dashboard',function(req,res,next){
     Food.find({}).populate('restaurant').sort({"modified" : -1}).limit(6).exec(function(err,food){
         FoodType.find().exec(function (err,foodtype){
             Restaurant.find({},function (err,restaurant){
-                console.log(typeof JSON.stringify(restaurant));
-                res.render('dashboard',{
-                    user:req.user,
-                    food:food,
-                    foodtype:foodtype,
-                    restaurant:restaurant,
-                    restoran:JSON.stringify(restaurant)
+                Order.find({'customer':req.user._id,status:true},function (err,order)
+                {
+                    res.render('dashboard',{
+                        user:req.user,
+                        food:food,
+                        foodtype:foodtype,
+                        restaurant:restaurant,
+                        restoran:JSON.stringify(restaurant),
+                        order:order.length
+                    });
                 });
+
             })
         })
     });
 });
 router.get('/order/:id',function (req,res,next){
 
+});
+
+router.post('/add-order/:id/restaurant/:restaurantID',function (req,res,next){
+    const foodID = req.params.id;
+    const userID = req.user._id;
+    const restaurantID = req.params.restaurantID;
+    const order = new Order({
+        customer:userID,
+        restaurant:restaurantID,
+        food:foodID,
+        status:true
+    });
+    order.save();
 });
 
 module.exports = router;
